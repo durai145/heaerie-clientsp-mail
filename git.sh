8654e327900ea8d32ef30db6daaf337db8e92e1c
duraimurugan Govindaraj <durai145@live.in>
2018-10-09 23:12:34 -0400

    initial

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "# heaerie-clientsp-mail" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/durai145/heaerie-clientsp-mail.git
+git push -u origin master
